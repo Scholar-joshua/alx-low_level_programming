786557dd05d534b1f8ce24c5b65ef8962ba376cd
Oriade Joshua <joshuaoluwafemi997@gmail.com>
2023-03-21 08:38:30 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - prints alpha 10 times
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int ten;
 	char la;
@@ -12,6 +12,6 @@ void print_alphabet_x10(void)
 	{
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
-		_putchar('\n')'
+		_putchar('\n');
 	}
 }
