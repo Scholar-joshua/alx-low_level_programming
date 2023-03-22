486770272d896884739bb641c51d55fada688de0
Oriade Joshua <joshuaoluwafemi997@gmail.com>
2023-03-22 12:29:58 -0700

    task 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,8 +10,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-void times_table(void);
+int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 int _putchar(char c);
 
 #endif
